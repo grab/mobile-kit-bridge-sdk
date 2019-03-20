45d5fc64aac4c440489d6c78b0b5d0d077dbb7e6
Viet Hai Pham <viethai.pham@grabtaxi.com>
2019-03-21 01:46:58 +0800

    chore(build): build UMD for testing in browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 // @ts-check
+const path = require("path");
 const gulp = require("gulp");
+const { sync } = require("del");
 const bundle = require("@lernetz/gulp-typescript-bundle");
 
 const srcPaths = {
@@ -7,17 +9,35 @@ const srcPaths = {
   src: ["src/**/*", "!**/*.test.*"]
 };
 
+const dest = "dist";
+
+gulp.task("clean:dist", async function() {
+  return sync(dest, { force: true });
+});
+
 gulp.task(
   "ts:bundle",
   bundle({
-    dest: "dist",
+    dest,
     src: "src/index.ts",
     name: "index",
-    rollup: { outputOptions: { sourcemap: false } }
+    rollup: {
+      outputOptions: { compact: false, format: "cjs", sourcemap: false }
+    }
   })
 );
 
-const distSequence = ["ts:bundle"];
+gulp.task(
+  "ts:bundle:umd",
+  bundle({
+    dest,
+    src: "src/index.ts",
+    name: "index.umd",
+    rollup: { outputOptions: { format: "umd", sourcemap: false } }
+  })
+);
+
+const distSequence = ["clean:dist", "ts:bundle", "ts:bundle:umd"];
 
 gulp.task("watch:src", function() {
   gulp.watch(
