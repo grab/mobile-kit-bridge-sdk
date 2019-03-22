47d7c23d87f980f38fed923428ef29bb80eb8934
Viet Hai Pham <viethai.pham@grabtaxi.com>
2019-03-22 18:30:06 +0800

    chore(build): add ts-essentials

--- dist/setup-global.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Set up global callback to handle multiple request IDs.
- * @param globalObject The global object - generally window.
- * @param param1 The required parameters.
- */
-export default function (globalObject: any, { callbackNameFunc }: Readonly<{
-    /** Get the name of the relevant callback. */
-    callbackNameFunc: (requestID: number | string | null) => string;
-}>): void;
